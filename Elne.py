95c48205fd55bba630430516f703e1fb771d7b97
Panagiotis Kourgiounis <60981745+pankourgiou@users.noreply.github.com>
2024-01-27 04:45:13 +0200

    Update Elne.py

--- Elne.py
+++ Elne.py
@@ -1,5 +1,5 @@
 #In this file we will be multiplying many mathematical formulas with given Energy formula. The result will be like we multiply both sides by 1 but we will raise the complexity of it...it's like decorating.
 #lne=1
-print("E*lne = lne*mcmod2 = lne*mc**2 = lne*gamma*mcmod2 = E")
+#print("E*lne = lne*mcmod2 = lne*mc**2 = lne*gamma*mcmod2 = E")
 #Given Electric potential V = kq/r where k = Coulomb's constant q = electric charge and r = distance from the charge to the point where the potential is measured we have
-print("E*lne*V= lne*mcmod2*(kq/r) = lne*gamma*mcmod2*(kq/r)")
+#print("E*lne*V= lne*mcmod2*(kq/r) = lne*gamma*mcmod2*(kq/r)")
